de793656fdb160e22b154f7ab1a6abf468849c00
Bismvrckz <Bismvrckz@github.com>
2022-09-02 14:10:52 +0700

    landing part 1

---
+++ packages/client/components/Navbar/index.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import Image from 'next/image';
+
+function Navbar() {
+  return (
+    <div className="h-[100px] flex items-end">
+      <div className="flex w-[100%] h-[70px] items-center justify-between">
+        <div className="flex ml-[7vw] h-[70%] items-center">
+          <Image
+            src="/Medbox.png"
+            alt="medbox-logo"
+            layout="fixed"
+            width={136}
+            height={32}
+          />
+        </div>
+        <div className="flex mr-[7vw] h-[70%] items-center w-[25vw] justify-between">
+          <Image
+            src="/Bell.png"
+            alt="bell-logo"
+            layout="fixed"
+            width={16.8}
+            height={19.2}
+          />
+          <Image
+            src="/Cart.png"
+            alt="cart-logo"
+            layout="fixed"
+            width={19.2}
+            height={20.4}
+          />
+          <Image
+            src="/Menu.png"
+            alt="menu-logo"
+            layout="fixed"
+            width={18}
+            height={12}
+          />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Navbar;
---
+++ packages/client/tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
+  ],
+  theme: {
+    extend: {},
+    screens: {
+      desktop: '1440px',
+      // => @media (min-width: 1440px) { ... }
+    },
+  },
+  plugins: [],
+};
