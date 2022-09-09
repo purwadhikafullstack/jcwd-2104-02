1e92cdc40d0b71f6fa28a96bfafa4b6d7790b08d
Purwadhika Fullstack <93514408+purwadhikafullstack@users.noreply.github.com>
2022-09-09 10:46:15 +0700

    Update and rename deploy-fe.config.js to deploy.config.js

---
+++ deploy.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  apps: [
+    {
+      name: 'JCWD-2104-02', // Format JCWD-{batchcode}-{groupnumber}
+      script: 'npm',
+      args: 'start',
+      time: true,
+    },
+  ],
+};
