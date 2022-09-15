e5d37ad3a68ec0be6da1dd24632b228bb1e81f6f
farhaanmhmmd <farhanmhmmd96@gmail.com>
2022-09-16 00:34:56 +0700

    Create Key

--- packages/client/components/EditAddress/index.js
+++ packages/client/components/EditAddress/index.js
@@ -122,7 +122,7 @@ function EditAddress(props) {
   const fetchProvince = async () => {
     try {
       const resGetProvince = await axiosInstance.get('/rajaongkir/provinsi');
-      setGetProvince(resGetProvince?.data?.rajaongkir?.results);
+      setGetProvince(resGetProvince.data.rajaongkir.results);
     } catch (error) {
       console.log({ error });
     }
