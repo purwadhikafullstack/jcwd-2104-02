3cb0f8ada928f16a172c36c70684f44eac840412
Bismvrckz <103980780+Bismvrckz@users.noreply.github.com>
2022-09-19 15:20:08 +0700

    Admin Landing (#13)
    Co-authored-by: Bismvrckz <Bismvrckz@github.com>

---
+++ packages/client/pages/admin/transaksi.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import AdminNavbar from '../../components/AdminNavbar';
+import { useRouter } from 'next/router';
+
+function Transaksi() {
+  const router = useRouter();
+
+  const path = router.pathname;
+
+  return (
+    <div className="flex w-[100vw] h-[100vh]">
+      <AdminNavbar path={path} />
+      <div>Kanan</div>
+    </div>
+  );
+}
+
+export default Transaksi;
