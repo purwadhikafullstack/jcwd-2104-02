c42b9414d9edd86bafbd74054c0937ef48ec4920
Farhan Muhammad <farhanmhmmd96@gmail.com>
2022-09-29 01:41:28 +0700

    Create All User Transaction History Page for Admin

---
+++ packages/client/components/AdminTransCard/index.js
@@ -0,0 +1,94 @@
+import {
+  Button,
+  Box,
+  HStack,
+  Text,
+  VStack,
+  useDisclosure,
+} from '@chakra-ui/react';
+import Image from 'next/image';
+import TransactionDetails from '../AdminDetailTrans';
+
+export default function AdminTransCard(props) {
+  const {
+    trans_id,
+    productName,
+    totalPrice,
+    status,
+    productImage,
+    courier,
+    deliveryCost,
+    createdAt,
+  } = props;
+  const { isOpen, onOpen, onClose } = useDisclosure();
+
+  const rawStatus = status.split('_');
+  return (
+    <Box>
+      <Text fontWeight={600} marginBottom={1} marginLeft={'81'}>
+        No. Invoice: {trans_id}
+      </Text>
+      <HStack
+        bg={'gray.50'}
+        height={125}
+        spacing="120"
+        marginBottom={6}
+        ml={'70'}
+        px={8}
+        rounded={6}
+        boxShadow="lg"
+      >
+        <Image
+          unoptimized
+          alt="resep-logo"
+          width={70}
+          height={70}
+          src={productImage}
+        />
+        <VStack>
+          <Text w="200px" fontSize={16} fontWeight={600}>
+            {productName}
+          </Text>
+          <Text w="200px" fontSize={14} fontWeight={450}>
+            Total Harga: Rp. {totalPrice.toLocaleString('id')}
+          </Text>
+          <Button
+            color="linkedin.500"
+            variant="link"
+            paddingRight={95}
+            fontSize={13}
+            fontWeight={500}
+            onClick={onOpen}
+          >
+            Detail Transaksi
+            <TransactionDetails
+              isOpen={isOpen}
+              onClose={onClose}
+              key={trans_id}
+              productName={productName}
+              productImage={productImage}
+              status={status}
+              totalPrice={totalPrice}
+              trans_id={trans_id}
+              courier={courier}
+              deliveryCost={deliveryCost}
+              createdAt={createdAt}
+              props={props}
+            />
+          </Button>
+        </VStack>
+        <Box w="200px" fontSize="15" fontWeight={500}>
+          Status: {rawStatus.join(' ')}
+        </Box>
+        <VStack textAlign="center" fontWeight={500}>
+          <Text w="150px" fontSize="13">
+            Tanggal Pemesanan:
+          </Text>
+          <Text w="150px" fontSize="13">
+            {createdAt.slice(0, 10)}
+          </Text>
+        </VStack>
+      </HStack>
+    </Box>
+  );
+}
--- packages/client/pages/admin/transaksi.js
+++ packages/client/pages/admin/transaksi.js
@@ -1,18 +1,146 @@
-import React from 'react';
 import AdminNavbar from '../../components/AdminNavbar';
 import { useRouter } from 'next/router';
+import {
+  Text,
+  Tabs,
+  TabList,
+  TabPanels,
+  TabPanel,
+  Tab,
+  VStack,
+} from '@chakra-ui/react';
+import { getSession } from 'next-auth/react';
+import { useState, useEffect } from 'react';
+import AdminTransCard from '../../components/AdminTransCard';
+import axiosInstance from '../../src/config/api';
 
-function Transaksi() {
+function Transaksi(props) {
   const router = useRouter();
 
   const path = router.pathname;
 
+  const [transac, setTransac] = useState([]);
+
+  const [selected, setSelected] = useState(0);
+
+  useEffect(() => {
+    fetchTransactions();
+  }, [selected]);
+
+  const fetchTransactions = async () => {
+    try {
+      const session = await getSession();
+
+      const { user_token } = session.user;
+
+      const config = {
+        headers: { Authorization: `Bearer ${user_token}` },
+      };
+      console.log({ selected });
+      const res = await axiosInstance.get(
+        `/transactions/admin/transactionsByIndex/${selected}`,
+        config,
+      );
+      setTransac(res.data.data.resFetchTransactions);
+    } catch (error) {
+      alert(error.message);
+    }
+  };
+
+  function mappedTransactions() {
+    return transac?.map((transaction, index) => {
+      return (
+        <AdminTransCard
+          key={transaction.transaction_id}
+          productName={transaction.transaction_details[0].product.productName}
+          productImage={transaction.transaction_details[0].product.productImage}
+          status={transaction.status}
+          totalPrice={transaction.totalPrice}
+          trans_id={transaction.transaction_id}
+          courier={transaction.courier}
+          deliveryCost={transaction.deliveryCost}
+          createdAt={transaction.createdAt}
+          props={props}
+        />
+      );
+    });
+  }
+
   return (
     <div className="flex w-[100vw] h-[100vh]">
       <AdminNavbar path={path} />
-      <div>Kanan</div>
+      <VStack align="start">
+        <Text
+          fontSize={23}
+          fontWeight={500}
+          marginTop={6}
+          marginBottom={3}
+          marginLeft={16}
+        >
+          Riwayat Transaksi
+        </Text>
+        <Tabs onChange={(index) => setSelected(index)}>
+          <TabList marginLeft={16} marginBottom={2}>
+            <Tab>Semua</Tab>
+            <Tab>Diproses</Tab>
+            <Tab>Sedang Dikirim</Tab>
+            <Tab>Berhasil</Tab>
+            <Tab>Tidak Berhasil</Tab>
+            <Tab>Menunggu Pembayaran</Tab>
+            <Tab>Menunggu Konfirmasi Pembayaran</Tab>
+          </TabList>
+          <TabPanels>
+            <TabPanel>
+              <div>{mappedTransactions()}</div>
+            </TabPanel>
+            <TabPanel>
+              <div>{mappedTransactions()}</div>
+            </TabPanel>
+            <TabPanel>
+              <div>{mappedTransactions()}</div>
+            </TabPanel>
+            <TabPanel>
+              <div>{mappedTransactions()}</div>
+            </TabPanel>
+            <TabPanel>
+              <div>{mappedTransactions()}</div>
+            </TabPanel>
+            <TabPanel>
+              <div>{mappedTransactions()}</div>
+            </TabPanel>
+            <TabPanel>
+              <div>{mappedTransactions()}</div>
+            </TabPanel>
+          </TabPanels>
+        </Tabs>
+      </VStack>
     </div>
   );
+}
+
+export async function getServerSideProps(context) {
+  try {
+    const session = await getSession({ req: context.req });
+
+    if (!session) return { redirect: { destination: '/login' } };
+
+    const { user_token } = session.user;
+
+    const config = {
+      headers: { Authorization: `Bearer ${user_token}` },
+    };
+    const { user_id } = context.params;
+
+    return {
+      props: {
+        user_id,
+        user_token,
+      },
+    };
+  } catch (error) {
+    console.log({ error });
+    return { props: {} };
+  }
 }
 
 export default Transaksi;
